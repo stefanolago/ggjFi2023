f692ce9242ea9e6afff8e6e8a4a1d968e70fee2c
xGiordan23x <francigio23@gmail.com>
2023-02-03 22:50:29 +0100

    prova mana

---
+++ Assets/francesco/Scripts/ManaControl.cs
@@ -0,0 +1,33 @@
+using JetBrains.Annotations;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ManaControl : MonoBehaviour
+{
+    public int maxMana;
+    public int currentMana;
+    public int distanceToToggleOneMana;
+    public float rootLenght = 0;
+    public DrawManager drawManager;
+
+    public bool temp;
+    
+
+    private void Update()
+    {
+        if(temp)
+        {
+            float[] pointDistances = drawManager.fingerPointsDistances.ToArray();
+            float totalDistances = pointDistances.Sum();
+            //calcolo togli punti
+
+            temp= false;
+            
+        }
+    }
+
+
+
+
+}
