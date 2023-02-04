150289ca121128cfa363c607cd0789b5a31c012b
xGiordan23x <francigio23@gmail.com>
2023-02-04 01:52:20 +0100

    inizio enemy

---
+++ Assets/Enemy.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float shootTime;
+    void Update()
+    {
+        
+    }
+}
