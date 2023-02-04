c23ba0ec6e9353f922b1eba9f6fcbf559ad943ac
stefanolago <jafarrolo@gmail.com>
2023-02-04 01:18:42 +0100

    Update RootGenerator.cs

--- Assets/Scripts/RootGenerator.cs
+++ Assets/Scripts/RootGenerator.cs
@@ -17,6 +17,10 @@ public class RootGenerator : MonoBehaviour
         currentLine = Instantiate(rootPrefab, Vector3.zero, Quaternion.identity);
         lineRenderer = currentLine.GetComponent<LineRenderer>();
         edgeCollider = currentLine.GetComponent<EdgeCollider2D>();
+
+        lineRenderer.SetPosition(0, rootPoints[0]);
+        lineRenderer.SetPosition(1, rootPoints[1]);
+        edgeCollider.points = rootPoints.ToArray();
     }
 
     // Update is called once per frame
@@ -30,3 +34,68 @@ public class RootGenerator : MonoBehaviour
 
     }
 }
+
+/*
+ * 
+ * public class DrawManager : MonoBehaviour
+{
+
+    public float maxMana = 100;
+    public float currentMana;
+    public GameObject rootPrefab;
+    public GameObject currentLine;
+    public LineRenderer lineRenderer;
+    public EdgeCollider2D edgeCollider;
+    public List<Vector2> fingerPositions;
+
+    private void Start()
+    {
+        currentMana = maxMana;
+    }
+    private void Update()
+    {
+        //Debug.Log(Input.mousePosition);
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            CreateLine();
+            Debug.Log("Creazione");
+        }
+        if (Input.GetMouseButton(0))
+        {
+            Vector2 tempFingerPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Debug.Log(tempFingerPos);
+
+            if (Vector2.Distance(tempFingerPos, fingerPositions[fingerPositions.Count-1]) > 0.1f)
+            {
+                UpdateLine(tempFingerPos);
+            }
+        }
+    }
+    public void CreateLine()
+    {
+        currentLine = Instantiate(rootPrefab,Vector3.zero,Quaternion.identity);
+        lineRenderer = currentLine.GetComponent<LineRenderer>();
+        edgeCollider = currentLine.GetComponent<EdgeCollider2D>();
+        fingerPositions.Clear();
+        Invoke(nameof(AddPosition),0.1f);
+        lineRenderer.SetPosition(0, fingerPositions[0]);
+        lineRenderer.SetPosition(1, fingerPositions[1]);
+        edgeCollider.points = fingerPositions.ToArray();
+    }
+    void UpdateLine(Vector2 newFingerPos)
+    {
+        fingerPositions.Add(newFingerPos);
+        lineRenderer.positionCount++;
+        lineRenderer.SetPosition(lineRenderer.positionCount -1, newFingerPos);
+        edgeCollider.points = fingerPositions.ToArray();
+    }
+    public void AddPosition()
+    {
+        fingerPositions.Add(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+    }
+}
+ * 
+ * 
+ * 
+ */
