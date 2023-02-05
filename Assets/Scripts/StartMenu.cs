68f4f3ef90b42d0705d0241a2567414af3622546
stefanolago <jafarrolo@gmail.com>
2023-02-05 13:55:14 +0100

    added start screen

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -7,7 +7,7 @@ public class StartMenu : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene("ProvaSte");
+        SceneManager.LoadScene("Livello Completo");
     }
 
     public void OptionsMenu()
